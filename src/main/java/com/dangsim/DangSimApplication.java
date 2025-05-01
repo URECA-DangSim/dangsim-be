46068dbf71561ef5bfb249191c9046e3cd14a2cb
dudxo <father8475@naver.com>
2025-05-01 22:53:23 +0900

    feat: project init

---
+++ src/main/java/com/dangsim/DangSimApplication.java
@@ -0,0 +1,13 @@
+package com.dangsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangSimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangSimApplication.class, args);
+	}
+
+}
