dcab4dcf2373497758ae05f1309e75a0ca737437
dudxo <father8475@naver.com>
2025-05-02 11:34:30 +0900

    feat: baseEntity, jpaAuditing

---
+++ src/main/java/com/dangsim/common/config/AuditAwareImpl.java
@@ -0,0 +1,28 @@
+package com.dangsim.common.config;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.dangsim.user.entity.User;
+
+public class AuditAwareImpl implements AuditorAware<String> {
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+		if (null == authentication || !authentication.isAuthenticated()) {
+			return Optional.empty();
+		}
+
+		Object principal = authentication.getPrincipal();
+		if(principal instanceof User) {
+			User user = (User) principal;
+			return Optional.of(user.getNickname());
+		}
+
+		return Optional.empty();
+	}
+}
