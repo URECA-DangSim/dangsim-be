1f39a4a734284db21f242da8688cb6af724fc1fa
dudxo <father8475@naver.com>
2025-05-05 01:15:36 +0900

    test #3: Task 상세 조회 Unit Test

---
+++ src/test/java/com/dangsim/task/service/TaskServiceTest.java
@@ -0,0 +1,133 @@
+package com.dangsim.task.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.dangsim.common.exception.runtime.BaseException;
+import com.dangsim.common.fixture.TaskFixture;
+import com.dangsim.common.fixture.UserFixture;
+import com.dangsim.task.dto.response.TaskDetailsResponseDto;
+import com.dangsim.task.entity.Task;
+import com.dangsim.task.exception.TaskErrorCode;
+import com.dangsim.task.repository.TaskRepository;
+import com.dangsim.user.entity.Role;
+import com.dangsim.user.entity.User;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskServiceTest {
+
+	@Mock
+	TaskRepository taskRepository;
+
+	@InjectMocks
+	TaskService taskService;
+
+	@DisplayName("일치하는 심부름 요청이 없으면 예외가 발생한다.")
+	@Test
+	void throwNotFoundExceptionWhenNotExistTaskByTaskId() {
+		// given
+		Long taskId = 1L;
+		User user = UserFixture.user(Role.USER, BigDecimal.ONE);
+
+		// when
+		given(taskRepository.findById(any(Long.class))).willReturn(Optional.empty());
+
+		// then
+		assertThatThrownBy(() -> taskService.getTaskById(taskId, user))
+			.isInstanceOf(BaseException.class)
+			.hasMessage(TaskErrorCode.NOT_FOUND_TASK.getMessage());
+	}
+
+	@DisplayName("해당하는 심부름 요청 상세 정보를 가져온다.")
+	@Test
+	void getTaskDetailsByTaskId() {
+		// given
+		final String title = "test1";
+		final String content = "content1";
+		User requester = UserFixture.user(Role.USER, BigDecimal.ONE);
+		ReflectionTestUtils.setField(requester, "id", 1L);
+
+		Task task = TaskFixture.task(title, content, requester);
+		ReflectionTestUtils.setField(task, "id", 1L);
+
+		given(taskRepository.findById(any(Long.class))).willReturn(Optional.of(task));
+
+		// when
+		TaskDetailsResponseDto responseDto = taskService.getTaskById(task.getId(), requester);
+
+		// then
+		assertAll(
+			() -> assertThat(responseDto.taskId()).isEqualTo(task.getId()),
+			() -> assertThat(responseDto.title()).isEqualTo(task.getTitle()),
+			() -> assertThat(responseDto.content()).isEqualTo(task.getContent())
+		);
+	}
+
+	@DisplayName("심부름 요청 정보를 가져올 때 자신의 요청인지 검증한다.")
+	@Test
+	void getTaskDetailsByTaskIdWithOwner() {
+		// given
+		final String title = "test1";
+		final String content = "content1";
+		User requester = UserFixture.user(Role.USER, BigDecimal.ONE);
+		ReflectionTestUtils.setField(requester, "id", 1L);
+
+		Task task = TaskFixture.task(title, content, requester);
+		ReflectionTestUtils.setField(task, "id", 1L);
+
+		given(taskRepository.findById(any(Long.class))).willReturn(Optional.of(task));
+
+		// when
+		TaskDetailsResponseDto responseDto = taskService.getTaskById(task.getId(), requester);
+
+		// then
+		assertAll(
+			() -> assertThat(responseDto.taskId()).isEqualTo(task.getId()),
+			() -> assertThat(responseDto.title()).isEqualTo(task.getTitle()),
+			() -> assertThat(responseDto.content()).isEqualTo(task.getContent()),
+			() -> assertThat(responseDto.isMyTask()).isTrue()
+		);
+	}
+
+	@DisplayName("심부름 요청 정보를 가져올 때 자신의 요청인지 검증한다.")
+	@Test
+	void getTaskDetailsByTaskIdWithOther() {
+		// given
+		final String title = "test1";
+		final String content = "content1";
+		User requester = UserFixture.user(Role.USER, BigDecimal.ONE);
+		User other = UserFixture.user(Role.USER, BigDecimal.ONE);
+		ReflectionTestUtils.setField(requester, "id", 1L);
+		ReflectionTestUtils.setField(other, "id", 2L);
+
+		Task task = TaskFixture.task(title, content, requester);
+		ReflectionTestUtils.setField(task, "id", 1L);
+
+		given(taskRepository.findById(any(Long.class))).willReturn(Optional.of(task));
+
+		// when
+		TaskDetailsResponseDto responseDto = taskService.getTaskById(task.getId(), other);
+
+		// then
+		assertAll(
+			() -> assertThat(responseDto.taskId()).isEqualTo(task.getId()),
+			() -> assertThat(responseDto.title()).isEqualTo(task.getTitle()),
+			() -> assertThat(responseDto.content()).isEqualTo(task.getContent()),
+			() -> assertThat(responseDto.isMyTask()).isFalse()
+		);
+	}
+
+}
