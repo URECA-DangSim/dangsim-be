21b4d1dcdc87e6494ba81bc43aa53f95eae31afd
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-05-07 16:26:08 +0900

    feat #7: ChatRoom Create API 추가

--- src/main/java/com/dangsim/chat/controller/ChatRoomController.java
+++ src/main/java/com/dangsim/chat/controller/ChatRoomController.java
@@ -1,35 +1,33 @@
 package com.dangsim.chat.controller;
 
-import com.dangsim.chat.dto.request.CreateChatRoomRequest;
-import com.dangsim.chat.dto.response.ChatRoomResponse;
-import com.dangsim.chat.entity.ChatRoom;
-import com.dangsim.chat.service.ChatRoomService;
-import com.dangsim.user.entity.User;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dangsim.chat.dto.request.CreateChatRoomRequest;
+import com.dangsim.chat.dto.response.ChatRoomResponse;
+import com.dangsim.chat.service.ChatRoomService;
+import com.dangsim.user.entity.User;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatRoomController {
 
-    private final ChatRoomService chatRoomService;
+	private final ChatRoomService chatRoomService;
 
-    @PostMapping("/api/chat-rooms/chat-room")
-    public ResponseEntity<ChatRoomResponse> createChatRoom(
-            @Valid @RequestBody
-            CreateChatRoomRequest request,
-            @AuthenticationPrincipal User performer
-    ){
-        //chatRoomService.createChatRoom(request, performer);
-        return null;
-    }
-
+	@PostMapping("/api/chat-rooms/chat-room")
+	public ResponseEntity<ChatRoomResponse> createChatRoom(
+		@Valid @RequestBody
+		CreateChatRoomRequest request,
+		@AuthenticationPrincipal User performer
+	) {
+		ChatRoomResponse response = chatRoomService.createChatRoom(request, performer);
+		return ResponseEntity.ok(response);
+	}
 
 }
--- src/main/java/com/dangsim/chat/dto/ChatRoomMapper.java
+++ src/main/java/com/dangsim/chat/dto/ChatRoomMapper.java
@@ -1,6 +1,14 @@
 package com.dangsim.chat.dto;
 
+import com.dangsim.chat.dto.response.ChatRoomResponse;
+import com.dangsim.chat.entity.ChatRoom;
+
 public class ChatRoomMapper {
 
+    public static ChatRoomResponse toChatRoomResponse(
+            ChatRoom chatRoom
+    ){
+        return new ChatRoomResponse(chatRoom.getId());
+    }
 
 }
