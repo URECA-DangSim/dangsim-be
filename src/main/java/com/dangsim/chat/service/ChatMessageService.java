fbadaae2de050d7acfbd8c73801a2784ddc8fae7
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-05-09 15:35:30 +0900

    feat #7: websocket 브로드캐스트 경로 추가

--- src/main/java/com/dangsim/chat/service/ChatMessageService.java
+++ src/main/java/com/dangsim/chat/service/ChatMessageService.java
@@ -1,25 +1,50 @@
 package com.dangsim.chat.service;
 
-import com.dangsim.chat.dto.request.ChatMessageRequest;
-import com.dangsim.chat.repository.ChatMessageRepository;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
-
-import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.dangsim.chat.dto.ChatMessageMapper;
+import com.dangsim.chat.dto.request.CreateChatMessageRequest;
+import com.dangsim.chat.dto.response.ChatMessageResponse;
+import com.dangsim.chat.entity.ChatMessage;
+import com.dangsim.chat.entity.ChatRoom;
+import com.dangsim.chat.exception.ChatRoomErrorCode;
+import com.dangsim.chat.repository.ChatMessageRepository;
+import com.dangsim.chat.repository.ChatRoomRepository;
+import com.dangsim.common.exception.runtime.BaseException;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ChatMessageService {
 
-    private final ChatMessageRepository chatMessageRepository;
+	private final ChatMessageRepository chatMessageRepository;
+	private final ChatRoomRepository chatRoomRepository;
+	private final SimpMessagingTemplate messagingTemplate;
 
-    @Transactional
-    public void createChatMessage(ChatMessageRequest request){
-        //서비스 구현
-    }
+	@Transactional
+	public void createChatMessage(@Valid CreateChatMessageRequest request, Long userId, Long chatRoomId) {
 
-    public void updateMessagesAsRead(Long chatRoomId, Long userId){
-        //메시지 읽음 처리 로직 작성 필요
-    }
+		ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(
+			() -> new BaseException(ChatRoomErrorCode.NOT_FOUND_CHATROOM)
+		);
 
+		if (!chatRoom.getPerformer().getId().equals(userId) && !chatRoom.getRequester().getId().equals(userId)) {
+			throw new BaseException(ChatRoomErrorCode.NOT_CHAT_ROOM_OWNER);
+		}
+
+		ChatMessage chatMessage = ChatMessage.of(chatRoomId, userId, request.content());
+		ChatMessage savedChatMessage = chatMessageRepository.save(chatMessage);
+
+		ChatMessageResponse response = ChatMessageMapper.toChatMessageResponse(savedChatMessage);
+
+		messagingTemplate.convertAndSend(
+			"/sub/chat-rooms/" + chatRoomId,
+			response
+		);
+
+	}
 }
