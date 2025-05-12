fa999ea66886b95eb200c8aeb887f7ed25845b6d
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-05-12 17:52:02 +0900

    feat #7: 채팅방 상세 조회 Service 추가

--- src/main/java/com/dangsim/chat/service/ChatRoomService.java
+++ src/main/java/com/dangsim/chat/service/ChatRoomService.java
@@ -8,13 +8,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.dangsim.chat.dto.ChatRoomMapper;
 import com.dangsim.chat.dto.request.CreateChatRoomRequest;
+import com.dangsim.chat.dto.response.ChatRoomDetailResponse;
+import com.dangsim.chat.dto.response.ChatRoomInfoResponse;
 import com.dangsim.chat.dto.response.ChatRoomResponse;
 import com.dangsim.chat.dto.response.ChatRoomSimpleResponse;
 import com.dangsim.chat.entity.ChatRoom;
+import com.dangsim.chat.exception.ChatRoomErrorCode;
 import com.dangsim.chat.repository.ChatRoomRepository;
 import com.dangsim.common.CursorPageResponse;
 import com.dangsim.common.exception.runtime.BaseException;
 import com.dangsim.common.util.DateTimeFormatUtils;
+import com.dangsim.task.dto.response.TaskInfoResponse;
 import com.dangsim.task.entity.Task;
 import com.dangsim.task.exception.TaskErrorCode;
 import com.dangsim.task.repository.TaskRepository;
@@ -52,5 +56,34 @@ public class ChatRoomService {
 		}
 
 		return chatRoomRepository.findChatRoomsByCursor(cursor, size, userId);
+	}
+
+	public CursorPageResponse<ChatRoomDetailResponse> getChatMessagesByCursor(Long chatRoomId, String cursor, int size,
+		Long userId) {
+		if (Objects.isNull(cursor) || cursor.isBlank()) {
+			cursor = DateTimeFormatUtils.formatDateTime(LocalDateTime.now());
+		}
+		return chatRoomRepository.findChatMessagesByCursor(chatRoomId, cursor, size, userId);
+	}
+
+	public ChatRoomInfoResponse getChatRoomInfo(Long chatRoomId, Long userId) {
+		ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
+			.orElseThrow(() -> new BaseException(ChatRoomErrorCode.NOT_FOUND_CHATROOM));
+
+		Task task = taskRepository.findById(chatRoom.getTask().getId())
+			.orElseThrow(() -> new BaseException(TaskErrorCode.NOT_FOUND_TASK));
+
+		TaskInfoResponse taskInfoResponse = ChatRoomMapper.toTaskInfoResponse(task);
+
+		Long chatPartnerId = (userId.equals(chatRoom.getRequester().getId()))
+			? chatRoom.getPerformer().getId()
+			: chatRoom.getRequester().getId();
+
+		User chatPartner = userRepository.findById(chatPartnerId)
+			.orElseThrow(() -> new BaseException(UserErrorCode.USER_NOT_FOUND));
+
+		return ChatRoomMapper.toChatRoomInfoResponse(chatRoomId, chatPartner,
+			taskInfoResponse);
+
 	}
 }
