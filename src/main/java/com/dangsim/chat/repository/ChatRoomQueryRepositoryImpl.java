dc5a326397cc9d1f545c2d3c56f68e727da7dd09
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-05-12 17:52:52 +0900

    feat #7: 채팅방 상세 조회 Query DSL 추가
    - chat message cursor 추가

--- src/main/java/com/dangsim/chat/repository/ChatRoomQueryRepositoryImpl.java
+++ src/main/java/com/dangsim/chat/repository/ChatRoomQueryRepositoryImpl.java
@@ -2,13 +2,17 @@ package com.dangsim.chat.repository;
 
 import static com.dangsim.chat.entity.QChatMessage.*;
 import static com.dangsim.chat.entity.QChatRoom.*;
-import static com.dangsim.user.entity.QUser.*;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.stereotype.Repository;
 
+import com.dangsim.chat.dto.response.ChatMessageDetailResponse;
+import com.dangsim.chat.dto.response.ChatRoomDetailResponse;
 import com.dangsim.chat.dto.response.ChatRoomSimpleResponse;
+import com.dangsim.chat.dto.response.QChatMessageDetailResponse;
 import com.dangsim.chat.dto.response.QChatRoomSimpleResponse;
 import com.dangsim.chat.entity.QChatMessage;
 import com.dangsim.common.CursorPageResponse;
@@ -32,7 +36,7 @@ public class ChatRoomQueryRepositoryImpl implements ChatRoomQueryRepository {
 		QUser partner = new QUser("partner");
 
 		List<ChatRoomSimpleResponse> chatRooms = queryFactory
-			.select(new QChatRoomSimpleResponse(chatRoom, chatMessage, user))
+			.select(new QChatRoomSimpleResponse(chatRoom, lastMessage, partner))
 			.from(chatRoom)
 			//최근 메시지 조인
 			.leftJoin(lastMessage)
@@ -46,7 +50,7 @@ public class ChatRoomQueryRepositoryImpl implements ChatRoomQueryRepository {
 			//상대방 유저 조인
 			.leftJoin(partner)
 			.on(
-				chatRoom.requester.id.eq(userId).and(partner.id.eq(chatRoom.requester.id))
+				chatRoom.requester.id.eq(userId).and(partner.id.eq(chatRoom.performer.id))
 					.or(chatRoom.performer.id.eq(userId).and(partner.id.eq(chatRoom.requester.id)))
 			)
 			.where(
@@ -58,8 +62,59 @@ public class ChatRoomQueryRepositoryImpl implements ChatRoomQueryRepository {
 			.fetch();
 
 		boolean hasNext = chatRooms.size() > size;
-		String nextCursor = hasNext ? chatRooms.get(size).timestamp() : null;
+		List<ChatRoomSimpleResponse> pageChatRooms = subLastPage(chatRooms, hasNext);
+		String nextCursor = getNextCursor(pageChatRooms, hasNext);
 
-		return new CursorPageResponse<>(chatRooms.stream().limit(size).toList(), nextCursor, hasNext);
+		return new CursorPageResponse<>(pageChatRooms, nextCursor, hasNext);
 	}
+
+	@Override
+	public CursorPageResponse<ChatRoomDetailResponse> findChatMessagesByCursor(Long chatRoomId, String cursor, int size,
+		Long userId) {
+
+		List<ChatMessageDetailResponse> allMessages = queryFactory
+			.select(new QChatMessageDetailResponse(chatMessage))
+			.from(chatMessage)
+			.where(
+				chatMessage.chatRoomId.eq(chatRoomId),
+				cursor != null ? chatMessage.createdAt.lt(DateTimeFormatUtils.parseDateTime(cursor)) : null
+			)
+			.orderBy(chatMessage.createdAt.desc())
+			.limit(size + 1)
+			.fetch();
+
+		boolean hasNext = allMessages.size() > size;
+		List<ChatMessageDetailResponse> pageMessages = subLastPage(allMessages, hasNext);
+		String nextCursor = getNextCursorByMessage(pageMessages, hasNext);
+
+		ChatRoomDetailResponse detailResponse = new ChatRoomDetailResponse(chatRoomId, pageMessages);
+
+		return new CursorPageResponse<>(Collections.singletonList(detailResponse), nextCursor, hasNext);
+	}
+
+	private String getNextCursor(List<ChatRoomSimpleResponse> chatrooms, boolean hasNext) {
+		if (Objects.isNull(chatrooms) || chatrooms.isEmpty() || !hasNext) {
+			return null;
+		}
+		return chatrooms.get(chatrooms.size() - 1).timestamp();
+	}
+
+	private String getNextCursorByMessage(List<ChatMessageDetailResponse> messages, boolean hasNext) {
+		if (Objects.isNull(messages) || messages.isEmpty() || !hasNext) {
+			return null;
+		}
+		return messages.get(messages.size() - 1).timeStamp();
+	}
+
+	private <T> List<T> subLastPage(List<T> list, boolean hasNext) {
+		if (Objects.isNull(list) || list.isEmpty()) {
+			return Collections.emptyList();
+		}
+
+		if (hasNext) {
+			return list.subList(0, list.size() - 1);
+		}
+		return list;
+	}
+
 }
