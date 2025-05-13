0e53e5bef558be021013fe969b61136fe3c65374
hayeong <110547143+hayong39@users.noreply.github.com>
2025-05-13 22:14:26 +0900

    사용자 심부름 요청/수행 내역 조회 API (#31)

--- src/main/java/com/dangsim/user/repository/UserRepository.java
+++ src/main/java/com/dangsim/user/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.dangsim.user.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, UserQueryRepository {
 	Optional<User> findByNickname(String nickname);
 
 	boolean existsByNicknameAndIdNot(String nickname, Long id);
