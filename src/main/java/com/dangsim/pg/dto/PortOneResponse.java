e4f42a70f86dd8d34ee230a10569392954e2243c
SOWON LEE <66356241+Leesowon@users.noreply.github.com>
2025-05-15 13:20:02 +0900

    refactor #34:불필요한 주석 제거

--- src/main/java/com/dangsim/pg/dto/PortOneResponse.java
+++ src/main/java/com/dangsim/pg/dto/PortOneResponse.java
@@ -1,12 +1,9 @@
 package com.dangsim.pg.dto;
 
-import java.math.BigDecimal;
-
 import lombok.Getter;
 
 @Getter
 public class PortOneResponse {
-//	private BigDecimal amount;
 	private String impUid;        // 아임포트 결제 고유번호
 	private String merchantUid;   // 주문 고유번호
 	private Long taskId;
